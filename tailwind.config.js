dcb88ef1932300fea951dd1a73bf6bda957b966d
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-09-16 15:37:33 -0300

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  purge: [
+      './public/**/*.html',
+  ],
+  darkMode: false,
+  theme: {
+      extend: {}
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+}
